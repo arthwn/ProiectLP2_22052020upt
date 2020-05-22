44baf0bab69c2059e5ebba7c442c5241eb77a503
arthwn <65404990+arthwn@users.noreply.github.com>
2020-05-22 13:55:10 +0300

    Update EQ.py

--- EQ.py
+++ EQ.py
@@ -1,6 +1,7 @@
 import numpy as np
 import wave
 import struct
+#import matplotlib.pyplot as plt
 #>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>INPUT>>>	
 fisier_wav = wave.open('test.wav', 'r')
 num_samples = fisier_wav.getnframes()
@@ -14,14 +15,40 @@ frecv = np.abs(data_transff[:len(data_transff)])
 
 print(frecv[1000])
 print(frecv[970])
+
+ncan = 1
+lat = fisier_wav.getsampwidth()
+samp_rate = 48000.0
+
+# plt.plot(frecv)
+# plt.title('Fecvente inainte de modificare')
+# plt.show()
+# plt.close()
 #>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>PROC>>>
 index = 0
+
+
 for f in frecv:
+    
     if index > 950 and index < 1050:
         frecv[index] = frecv[index] + 100
     index += 1
 
 print(frecv[1000])
 print(frecv[970])
+
+# plt.plot(frecv)
+# plt.title("Frecvente dupa modificare")
+# plt.xlim(0,1200)
+# plt.show()
+# plt.close()
 #>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>OUTPUT>>>
-#...
+
+fisier_nou = np.fft.ifft(frecv)
+
+output = wave.open('iesire.wav', 'w')
+
+output.setparams((ncan, lat, int(samp_rate), num_samples, 'NONE', 'not compressed'))
+
+for a in fisier_nou:
+    output.writeframes(struct.pack('h', int(a)))
